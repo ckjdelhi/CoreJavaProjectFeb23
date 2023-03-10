a2b8e8113b2ee25c8a31d1485d23a5ad2bbe9358
ckjdelhi <chandan.krk@gmail.com>
2023-03-10 08:25:32 +0530

    created new branch and added codes

--- src/com/string/StringDemo.java
+++ src/com/string/StringDemo.java
@@ -4,7 +4,7 @@ public class StringDemo {
 
 	public static void main(String[] args) {
 		System.out.println("hi");
-		
+		System.out.println("hi");
 //		String s1 ="abc";//using string literals
 //		String s2 ="abc";
 //		
